ea4a6b6636ec811f559d9e8a9e60ae4371eb177d
Skycat1983 <hlaoutaris@gmail.com>
2025-06-03 13:25:47 +0200

    background scripts and refs rmoved as unused

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,16 +3,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default [
-  // Background Script Bundle
-  {
-    input: "src/scripts/background/index.ts",
-    output: {
-      file: "dist/scripts/background.js",
-      format: "iife",
-    },
-    plugins: [typescript(), resolve(), commonjs()],
-  },
-
   // Content Script Bundle
   {
     input: "src/scripts/content/index.ts",
