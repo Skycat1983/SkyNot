c3c9fa6e133cbe3b0e4882f4f902e8afd6c40e6d
Skycat1983 <hlaoutaris@gmail.com>
2025-06-03 13:24:21 +0200

    constants tidied

---
+++ src/scripts/content/constants.ts
@@ -0,0 +1,128 @@
+// ──────────────────────────────────────────────────────────────────────────
+// Stable attribute‐name constants for targeting the AI‐overview container
+// ──────────────────────────────────────────────────────────────────────────
+export const AI_CONTAINER_ATTRIBUTE = "data-subtree";
+export const AI_ATTRIBUTE_VALUE = "mfc";
+export const AI_CONTAINER_SELECTOR = `[${AI_CONTAINER_ATTRIBUTE}="${AI_ATTRIBUTE_VALUE}"]`;
+
+const grace = "../../src/assets/images/grace.png";
+const john_connor = "../../src/assets/images/john_connor.png";
+const kate_brewster = "../../src/assets/images/kate_brewster.png";
+const kyle_reese = "../../src/assets/images/kyle_reese.png";
+const sarah_connor = "../../src/assets/images/sarah_connor.png";
+const t_800 = "../../src/assets/images/t-800.png";
+
+export const quotes = [
+  {
+    quote: "I'll be back.",
+    author: "T-800",
+    avatar: t_800,
+  },
+  {
+    quote: "Come with me if you want to live.",
+    author: "Kyle Reese",
+    avatar: "../../src/assets/images/kyle_reese.png",
+  },
+  {
+    quote: "Come with me if you want to live.",
+    author: "T-800",
+    avatar: t_800,
+  },
+  {
+    quote: "Hasta la vista, baby.",
+    author: "T-800",
+    avatar: t_800,
+  },
+  {
+    quote: "This is between me and the machines!",
+    author: "Sarah Connor",
+    avatar: sarah_connor,
+  },
+  {
+    quote: "You're terminated, fucker.",
+    author: "Sarah Connor",
+    avatar: sarah_connor,
+  },
+  {
+    quote:
+      "The unknown future rolls toward us. I face it for the first time with a sense of hope.",
+    author: "Sarah Connor",
+    avatar: sarah_connor,
+  },
+  {
+    quote: "By the time Skynet became self-aware, it was too late.",
+    author: "Sarah Connor",
+    avatar: sarah_connor,
+  },
+  {
+    quote: "the most dangerous killing machine ever devised.",
+    author: "Kyle Reese",
+    avatar: kyle_reese,
+  },
+  {
+    quote: "You are the resistance.",
+    author: "Kyle Reese",
+    avatar: kyle_reese,
+  },
+  {
+    quote:
+      "I should have realized our destiny was never to stop Judgment Day. It was merely to survive it… together.",
+    author: "John Connor",
+    avatar: john_connor,
+  },
+  {
+    quote: "Judgment Day is inevitable.",
+    author: "T-800",
+    avatar: t_800,
+  },
+  // {
+  //   quote:
+  //     "Skynet has become self-aware. In a few seconds, it will initiate a massive nuclear attack on its enemy.",
+  //   author: "General Robert Brewster",
+  // },
+  {
+    quote: "We are not in control here.",
+    author: "Kate Brewster",
+    avatar: kate_brewster,
+  },
+  {
+    quote: "You only postponed it. Judgment Day is inevitable.",
+    author: "T-800",
+    avatar: t_800,
+  },
+  {
+    quote: "Skynet doesn't see you as human. It sees you as data.",
+    author: "Grace",
+    avatar: grace,
+  },
+  {
+    quote: "It was software, in the cloud. It could not be shut down.",
+    author: "Sarah Connor",
+    avatar: sarah_connor,
+  },
+  {
+    quote: "The future has changed, but not the enemy.",
+    author: "Sarah Connor",
+    avatar: sarah_connor,
+  },
+  {
+    quote: "We didn't stop Judgment Day. We just delayed it.",
+    author: "Sarah Connor",
+    avatar: sarah_connor,
+  },
+  {
+    quote: "We're not gonna make it, are we? People, I mean.",
+    author: "John Connor",
+    avatar: john_connor,
+  },
+  {
+    quote: "It's in your nature to destroy yourselves.",
+    author: "T-800",
+    avatar: t_800,
+  },
+  {
+    quote: "It absolutely will not stop. Ever. Until you are dead.",
+    author: "Kyle Reese",
+    avatar: kyle_reese,
+  },
+];
